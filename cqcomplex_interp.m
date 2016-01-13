db6d91556d1aa604cef1cff52858983687dc307d
Chen <joe19890222@gmail.com>
2016-01-13 11:31:49 -0600

    Rewrite major part of the code with better understanding of the interpolation theory.  Change the function interface!!
    It is now more accurate and stable.

--- cqcomplex_interp.m
+++ cqcomplex_interp.m
@@ -1,64 +1,53 @@
-function [ coeffq ] = cqcomplex_interp( coeff, dt, fq, term )
+function [ coeffq ] = cqcomplex_interp( coeff, x, qval, term )
 % complex number interpolation using Fourier series
 % method referenced to Wade's UH Thesis, 1989
 %
 % input
 % -----
-% coeff = Fourier coefficient with length 2 * N, N must be integer and
-%         base of 2. The coeff is arranged in [0 ~ 2*pi) using matlab
-%         fft method. (DON'T use fftshift)
-% dt = sampling rate before fourier transform to get coeff
-% fq = query frequency (df = 1/dt)
-%      (fq must be lying within -1/2/dt to 1/2/dt)
+% coeff = Complex(or real) Numbers with length N
+% x = vector of x axis of coeff
+% qval = query values of coeff
 % term = 6 = how many terms you want to use to interpolate
 % 
 % output
 % ------
 % coeffq = complex value at the query frequency, it is a column vector
-%
-% Papare coeff
-% ------------
-% 1. time_series is a vector with length N
-% 2. coeff = fft(time_series,2*N)
-% 3. The principle is to make sure the coeff's length must be two
-%    times larger than the useful data length in time_series
+
 
 if ~exist('term','var')||isempty(term)
     term = 6;
 end
 
 % check input
-N_2 = length(coeff);
-N = N_2/2;
-if mod(N_2,2)~=0
-    error('Invalud coeff, its length should be even!');
+dx = x(2) - x(1);
+if length(x) ~= length(coeff)
+    error('Invalid x axis! Must equal to coeff length!');
 end
 
-fnyq = 1/2/dt;
-if max(fq) > fnyq || min(fq) < -fnyq
-    error('Invalid query frequency, it should not out-bound Nyquist!');
+% make sure the query x is within x limit
+if max(qval) > max(x) || min(qval) < min(x)
+    error('Invalid query position! Must within range of x!');
 end
 
-fq = fq(:);
-% recompose input data from [0,2*pi) to [-pi,pi]
-coeff = coeff(:); % convert to column vector
-% now coeff is from [-pi ~ pi]
-coeff = [coeff(end);coeff(N_2/2+2:end);coeff(1:N_2/2+1)];
-sgm = fq * 2 * N * dt;
+coeff = coeff(:);
+qval = qval(:);
+
+
 
 % find nearest term number of data to interpolate and do interpolation
 % point-by-point
-gma = -N:N;
-gma = gma(:); % integer index from 0 to 2N
-coeffq = zeros(length(fq),1);
-for k = 1:length(fq)
+coeffq = zeros(length(qval),1);
+for k = 1:length(qval)
     % sort the difference and select the minimum term
-    [~,I] = sort(abs(gma-sgm(k)));
+    float_index_distance = (x - qval(k))/dx;
+    [~,I] = sort(abs(float_index_distance));
     I = I(1:term);
-    gma_sgm = (gma(I) - sgm(k)); % gamma minus sigma times pi
+    float_index_distance = float_index_distance(I);
+    float_index_distance = float_index_distance(:);
     coeffq(k) = ...
-        sum( coeff(I) .* exp(1i*gma_sgm/2).*...
-        sinc(gma_sgm/4).^2 .* sinc(gma_sgm) );
+        sum( coeff(I) .* exp(1i*float_index_distance*pi/2) .* ...
+        sinc(float_index_distance/4).^2 .* ...
+        sinc(float_index_distance) );
     % Matlab sinc function: sin(pi*x)/(pi*x)
 end
 
